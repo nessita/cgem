80a540b31bee33ea849666f6fbbb07ff4d4ed323
nessita <nataliabidart@gmail.com>
2014-09-28 20:54:59 -0300

    Fixed style issues.

--- gemcore/management/commands/move_tags.py
+++ gemcore/management/commands/move_tags.py
@@ -1,7 +1,7 @@
 import operator
 from functools import reduce
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from gemcore.models import Entry
 
