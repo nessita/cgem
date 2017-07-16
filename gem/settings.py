e7b49eadb40f0280067b23941eec5246e866db65
Natalia <nataliabidart@gmail.com>
2017-07-16 11:40:41 -0300

    - Cleanup settings.

--- gem/settings.py
+++ gem/settings.py
@@ -85,15 +85,10 @@ WSGI_APPLICATION = 'gem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cgem',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+    # Parse database configuration from $DATABASE_URL
+    'default': dj_database_url.config(),
 }
 
 
@@ -127,9 +122,6 @@ USE_TZ = True
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-# Parse database configuration from $DATABASE_URL
-DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
