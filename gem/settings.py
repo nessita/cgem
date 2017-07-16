833b37804c5053572843d10fbbf985ddff3580ca
Natalia <nataliabidart@gmail.com>
2017-07-16 11:27:35 -0300

    - Tests passing.

--- gem/settings.py
+++ gem/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
