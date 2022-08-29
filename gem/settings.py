fe36134ef344afe62b18aec97c68d7829bc52b4e
Natalia <nataliabidart@gmail.com>
2022-08-29 15:24:21 -0300

    Debug

--- gem/settings.py
+++ gem/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DJANGO_DEBUG', False) in ('True', '1', 'on')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
