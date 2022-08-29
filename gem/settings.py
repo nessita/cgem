f2184c7d5156253d6e50b34a631047e521f0ce65
Natalia <nataliabidart@gmail.com>
2022-08-29 13:07:12 -0300

    Initial setup for integration with fly.io

--- gem/settings.py
+++ gem/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
